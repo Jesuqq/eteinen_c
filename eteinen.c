f5c9f05a17b237443800d013878ce39ade1ffb4e
Jesse <tdk.upper@gmail.com>
2015-09-17 14:31:52 +0300

    defined pin layout

--- eteinen.c
+++ eteinen.c
@@ -3,4 +3,16 @@
  *and to turn on relay controlling a light at defined raspberry pins
  * */
 
+// we are using wiringPi library
+#include <wiringPi.h>
 
+//defining pins used for motion sensor and relay
+#define MOTION = 13;
+#define RELAY = 11;
+
+
+int main (void)	{
+	wiringPiSetup();
+	pinMode(13, OUTPUT);
+	
+}
